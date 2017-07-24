e1f485a90533f733a0d4ddd28c52c7d73a75cb79
Joseph Kohatsu <jpkohatsu@gmail.com>
2017-07-24 18:07:47 -0500

    files updated

---
+++ keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+  spotify_id: '3528770a5bd84cc7a7eb01a3dd136bc6',
+  spotify_secret: '20587d672ca544639c0d85e14d8fa528',
+  twitter_token: '888155985235369994-Vtl72U5s6zfHwPW6D7ofUyIOjsxYbHA',
+  twitter_token_secret: 'HvNWS7fsIQftEJTCFB2zUEEAWRky7GvSTCie93a87SGtL',
+  twitter_key: 'OueOl8XK9vBNFlcZ29oz2Tqmf',
+  twitter_secret: 'BFmzjYv0Qv1exXm0cGkRgswBeEwkQAZbUfvxE0QluNt4uciXDs',
+
+}
+
